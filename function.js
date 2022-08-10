387f1674232088777b07280285d54ea13449ddf0
Hawwass1 <suleimanhawwa.sh@gmail.com>
2022-08-10 22:19:22 +0100

    added back buttons

--- function.js
+++ function.js
@@ -2,3 +2,17 @@ function next() {
     document.getElementById("Personal").style.display = "none";
     document.getElementById("Location").style.display = "flex";
 }
+function nextt() {
+    document.getElementById("Personal").style.display = "none";
+    document.getElementById("Location").style.display= "none";
+    document.getElementById("Education").style.display = "flex";
+}
+function back() {
+    document.getElementById("Location").style.display = "none";
+    document.getElementById("Personal").style.display = "flex";
+}
+function backk() {
+    document.getElementById("Personal").style.display = "none";
+    document.getElementById("Location").style.display= "flex";
+    document.getElementById("Education").style.display = "none";
+}
